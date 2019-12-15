eac9e3eee01972bc124600a1d717e973380ab902
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2019-12-15 11:21:31 +0200

    Update Flery.cpp

--- Flery.cpp
+++ Flery.cpp
@@ -1,26 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-queue <int> st;
-int v[100][100];
-bool marked[100] = {0};
+int a[107][107],m[107],n,s;
+stack <int> st;
+vector <int> res;
+
 int main(){
-int n;
 cin>>n;
-for(int i = 1;i<=n;i++){
-    int k = 0;
+for(int i=1;i<=n;i++)
     for(int j=1;j<=n;j++){
-        cin>>v[i][j];
-        k+=v[i][j];
+        cin>>a[i][j];}
+    st.push (1);
+    while (!st.empty()) {
+        int v = st.top();
+        int i;
+        for (i=1; i<=n; ++i)
+            if (a[v][i])
+                break;
+        if (i == n+1) {
+            res.push_back (v);
+            st.pop();
+        }
+        else {
+            --a[v][i];
+            --a[i][v];
+            st.push (i);
+        }
     }
-    if(k%2==1){cout<<"Wrong graph";return 0;}
-}
-
-st.push(1);
-while(st.size()!=0){
-
-    for(int i=1;i<=n;i++)
-    if(v[st.front()][i] == 1  && marked[i] == 0){marked[i] = 1;st.push(i);v[st.front()][i] = 0;v[i][st.front()] = 0;}
-    cout<<st.front()<<' ';
-    st.pop();
-}
+    for(int i=0;i<res.size();i++){
+        cout<<res[i]<<' ';
+    }
 }
